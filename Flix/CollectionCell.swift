9a92259ff30417c3a1836e21fcff0429e6ce6cb3
Daniela Gonzalez <danielagonzalez@fb.com>
2016-06-17 11:45:18 -0700

    collection view

---
+++ Flix/CollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionCell.swift
+//  Flix
+//
+//  Created by Daniela Gonzalez on 6/15/16.
+//  Copyright © 2016 Daniela Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class CollectionCell: UICollectionViewCell {
+    
+}
