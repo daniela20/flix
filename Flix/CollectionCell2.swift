a8370886cd656ffd9b068004acb9e3b88249bc10
Daniela Gonzalez <danielagonzalez@fb.com>
2016-06-17 16:06:42 -0700

    ok i have done enough

---
+++ Flix/CollectionCell2.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionCell2.swift
+//  Flix
+//
+//  Created by Daniela Gonzalez on 6/17/16.
+//  Copyright © 2016 Daniela Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class CollectionCell2: UICollectionViewCell {
+    
+}
---
+++ Pods/CollectionCell2CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionCell2CollectionViewCell.swift
+//  Pods
+//
+//  Created by Daniela Gonzalez on 6/17/16.
+//
+//
+
+import UIKit
+
+class CollectionCell2CollectionViewCell: UICollectionViewCell {
+    
+}
